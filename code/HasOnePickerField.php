c549d47870a73f5b06c804ae92318fbeccbe14d5
Brice Burgess <nesta@iceburg.net>
2013-06-28 18:16:15 -0500

    HasOnePickerField - clarify arguments and fix dataModelClass issue

--- code/HasOnePickerField.php
+++ code/HasOnePickerField.php
@@ -8,29 +8,29 @@ class HasOnePickerField extends PickerField {
 	 * Usage [e.g. in getCMSFields]
 	 *    $field = new HasOnePickerField($this, 'DamID', 'Selected Dam', $this->Dam(), 'Select a Dam');
 	 *    
-	 * @param DataObject $childObject   - The DataObject we are manipulating with this field
-	 * @param string $name              - Field Name of has_one relationship (e.g. DamID, SireID, etc.)
+	 * @param DataObject $childObject   - The DataObject we are manipulating with this field: typically $this via getCMSFields
+	 * @param string $name              - Field Name of has_one relationship (e.g. DamID, SireID, etc.): ensure 'ID' suffix
 	 * @param string $title             - GridField Title
-	 * @param SS_List $dataList         - Result of the has_one relationship method (E.g. $this->HasOneMethod())
+	 * @param Object $currentHasOne     - Result of the current has_one relationship method (E.g. $this->HasOneMethod())
 	 * @param string $linkExistingTitle - AddExisting Button Title
 	 */
 	
-	public function __construct(DataObject $childObject, $name, $title = null, SS_List $dataList, $linkExistingTitle = null, $searchContext = null) {
-		$modelClass = $dataList->className;
+	public function __construct(DataObject $childObject, $name, $title = null, Object $currentHasOne, $linkExistingTitle = null, $searchContext = null) {
+		$modelClass = $currentHasOne->className;
 		$this->childObject = $childObject;
 		$this->setModelClass($modelClass);
 		
 		// convert the has_one relation getter to a DataList / SS_List
-		// @todo is there a DataObject getter for a has_one relation that returns an appropriate DataList???
-		// @todo  ^^ for now, use the $childObject kludge.
-		$dataList = $modelClass::get()->filter(array('ID' => $dataList->ID));
-		$dataList->setDataModel($childObject);
+		$dataList = $modelClass::get()->filter(array('ID' => $currentHasOne->ID));
 		
+
 		// construct the PickerField
 		parent::__construct($name, $title, $dataList, $linkExistingTitle);
 		
+		
 		// remove components non-applicable to has_one relationships
 		$this->getConfig()->removeComponentsByType('GridFieldPaginator');
+		
 	}
 	
 }
